ee3350de77309c1cdbe8507f1d4833bc8afc8060
abused_master <techperson71@gmail.com>
2020-02-20 03:58:00 -0500

    Finished Login Screen UI

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,24 +1,70 @@
 import React from 'react';
-import {CssBaseline, MuiThemeProvider, withStyles} from "@material-ui/core";
-import {NavStyles} from './../styles/NavStyles';
+import {
+    CssBaseline,
+    MuiThemeProvider,
+    withStyles,
+    Card,
+    CardContent,
+    Typography,
+    TextField,
+    Button
+} from "@material-ui/core";
+import {Redirect} from 'react-router-dom'
 import {defaultTheme} from "./../styles/Theme";
-import {MaterialLoginStyles} from "../styles/LoginStyles";
+import {LoginStyles, MaterialLoginStyles} from "../styles/LoginStyles";
 
 class LoginPage extends React.Component {
 
     state = {
         email: '',
-        password: ''
+        password: '',
+        incorrectDetails: false,
+        loggedIn: false
     };
 
     render() {
+        let {email, password, incorrectDetails, loggedIn} = this.state;
         let {classes} = this.props;
+
+        if(loggedIn) {
+            return <Redirect to='/panel' />
+        }
 
         return (
             <MuiThemeProvider theme={defaultTheme}>
                 <CssBaseline />
-                <div style={NavStyles.body}>
-                    Hello World
+                <div style={LoginStyles.body}>
+                    <Card className={classes.loginCard}>
+                        <CardContent style={{textAlign: 'center'}}>
+                            <Typography variant='h4' color='textSecondary' noWrap>Login</Typography>
+                            {incorrectDetails ? <Typography variant='h6' color='error' noWrap={false}>Invalid Email or Password</Typography> : null}
+
+                            <form className={classes.loginForm}>
+                                <TextField
+                                    className={classes.textField}
+                                    inputProps={{className: classes.textProps}}
+                                    label='Email'
+                                    value={email}
+                                    id='standard-basic'
+                                    error={incorrectDetails}
+                                    onChange={(event) => this.setState({email: event.target.value})}
+                                />
+
+                                <TextField
+                                    className={classes.textField}
+                                    inputProps={{className: classes.textProps}}
+                                    label='Password'
+                                    value={password}
+                                    type='password'
+                                    id='standard-adornment-password'
+                                    error={incorrectDetails}
+                                    onChange={(event) => this.setState({password: event.target.value})}
+                                />
+
+                                <Button variant='contained' color='secondary' className={classes.loginBtn} onClick={() => this.setState({loggedIn: true})}>Login</Button>
+                            </form>
+                        </CardContent>
+                    </Card>
                 </div>
             </MuiThemeProvider>
         );
--- src/styles/LoginStyles.js
+++ src/styles/LoginStyles.js
@@ -1,8 +1,58 @@
 let LoginStyles = {
-
+    body: {
+        flexGrow: 1,
+        width: '100%',
+        height: '100%',
+        backgroundImage: 'url(/assets/images/login_bg.svg)',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 };
 
 let MaterialLoginStyles = theme => ({
+    loginCard: {
+        width: '80%',
+        height: 'auto',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10,
+        [theme.breakpoints.up('md')]: {
+            width: '20%',
+        }
+    },
+    loginForm: {
+        textAlign: 'center'
+    },
+    textField: {
+        width: '100%',
+        marginTop: 10,
+        '& label.Mui-focused': {
+            color: theme.palette.text.secondary
+        },
+        '& .MuiInput-underline:after': {
+            borderBottomColor: theme.palette.success.light
+        },
+        '&:hover .MuiInput-underline:before': {
+            borderColor: theme.palette.success.dark
+        }
+    },
+    textProps: {
+        color: theme.palette.text.secondary
+    },
+    loginBtn: {
+        backgroundColor: theme.palette.success.main,
+        marginTop: 20,
+        width: '40%',
+        '&:hover': {
+            backgroundColor: theme.palette.success.light
+        }
+    }
 });
 
 export {LoginStyles, MaterialLoginStyles}
